ab4aed9dd1e40b43de723892f5d17af2159f4f7e
alan.cao <zongying.cao@hpe.com>
2018-04-10 04:37:24 -0400

    spring can run

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,17 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	return application.sources(Application.class);
-    }
-    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
